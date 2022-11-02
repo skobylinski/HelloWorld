8fd9bd821109b7ce2299ef1c70499cbc9fa11d12
Lady Aga <Lady Aga@DESKTOP-DOITUFD>
2022-11-02 12:32:20 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //Przy tworzeniu nowej metody maksymalnie jedna linia odstępu
+    //Nowy plik powinien zawierać maksymalnie jedną klasę, dla kolejnych nowe pliki
+    //Warto dodawać modyfikator dostępu "public" przed elementami w celu ułatwienia przeszukiwania kodu
+
+    //PascalCase - sposób nazewnictwa dla nazw klas, plików, namespace, metod
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //camelCase - sposób nazewnictwa dla lokalnych zmiennych i prywatnych elemntów programów (niepublicznych)
+            string firstAndLastName = "Sebastian";
+
+            //UPPER_CASE - sposób nazewnictwa 
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Sebastian Kobyliński");
+            if (PI_NUMBER >2)
+            {
+            }
+        }
+    }
+}
